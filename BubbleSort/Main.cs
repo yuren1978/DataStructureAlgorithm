5b14d117e48c26746f0f94db7d807867c4a20596
Jianwei Sun <jianwei.sun@gmail.com>
2011-01-27 21:22:18 -0500

    add bubble sort

---
+++ BubbleSort/Main.cs
@@ -0,0 +1,41 @@
+using System;
+
+namespace BubbleSort
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			//Every loop, you need move the biggest element to the end of the list.
+			//so, next loop, you can only compare the first couple of elements.
+			int[] a={3,2,5,4,1};
+			BubbleSortArray(a);
+			for (int i = 0; i < a.Length; i++) {
+				Console.Write(a[i]);
+			}
+			Console.WriteLine();
+		}
+		
+		private static void BubbleSortArray(int[] a)
+		{
+			//http://www.algolist.net/Algorithms/Sorting/Bubble_sort
+			bool swapped=true;
+			int j=0;
+			while(swapped)
+			{
+				swapped=false;
+				j++;
+				for (int i = 0; i < a.Length-j; i++) {
+					if(a[i]>a[i+1])
+					{
+						int temp=a[i];
+						a[i]=a[i+1];
+						a[i+1]=temp;
+						swapped=true;
+					}
+				}
+			}			
+		}
+	}
+}
+
