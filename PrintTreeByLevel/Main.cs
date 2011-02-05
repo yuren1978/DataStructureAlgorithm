1e618c46f961678307a68d10459cc2ddf5c1124b
Jianwei Sun <jianwei.sun@gmail.com>
2011-02-04 22:37:45 -0500

    create skeleton

---
+++ PrintTreeByLevel/Main.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace PrintTreeByLevel
+{
+	//define a tree node.
+	class Node
+	{
+		int value;
+		Node leftChild;
+		Node rightChild;
+	}
+	
+	
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Start Program");
+			int[] a={1,2,3,4,5,6,7,8};
+			Console.WriteLine ("End Program");
+		}
+		
+		private Node CreateTree()
+		{
+		}
+		
+		private void PrintTreeByLeve()
+		{
+		}
+	}
+}
+
