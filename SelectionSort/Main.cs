501fb951eaa76fff032f0b56d50ea473979cc271
Jianwei Sun <jianwei.sun@gmail.com>
2011-02-21 06:46:20 -0500

    add select sort

---
+++ SelectionSort/Main.cs
@@ -0,0 +1,43 @@
+using System;
+
+
+namespace SelectionSort
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+			int[] a={2,5,3,1,8};
+			SelectSorting(a);
+			foreach (int item in a) {
+				Console.WriteLine(item);
+			}
+			
+		}
+	
+		public static void SelectSorting(int[] elements)
+		{
+			int length=elements.Length;
+			for(int index=0; index<length-1; index++)
+			{
+				int minElement=elements[index];
+				int minIndex=index;
+				for (int j = index+1; j < length; j++) {
+					if(elements[j]<minElement)
+					{
+						minElement=elements[j];
+						minIndex=j;
+					}
+				}
+				
+				//exchange element
+				int temp=elements[minIndex];
+				elements[minIndex]=elements[index];
+				elements[index]=temp;
+				
+			}
+		}
+	}
+}
+
