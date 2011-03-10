87519f708b08421be92f5b37e42e63e68d19c647
Jianwei Sun <jianwei.sun@gmail.com>
2011-03-09 21:41:31 -0500

    add unsigned integer problem

---
+++ IsUnsingedIntegerPowerOfTwo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IsUnsingedIntegerPowerOfTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint number1 = 1;
+            Console.WriteLine("number1 is the powr of " + IsPowerOfTwo(number1));
+        }
+
+        public static bool IsPowerOfTwo(uint number)
+        {
+            return (number & (number - 1))==0;
+        }
+    }
+}
