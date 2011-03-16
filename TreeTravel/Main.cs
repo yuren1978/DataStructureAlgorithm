c42c7f9f3a102104a57254d71b0d3811ef9102b1
Jianwei Sun <jianwei.sun@gmail.com>
2011-03-16 07:18:18 -0400

    add tree file

---
+++ TreeTravel/Main.cs
@@ -0,0 +1,42 @@
+using System;
+
+namespace TreeTravel
+{
+	class MainClass
+	{
+		private static int index=0;
+		public static void Main (string[] args)
+		{
+			//Console.WriteLine ("Hello World!");
+			int?[] a={1 ,2,null,3, null, null,4, 5, null, null, 6, null, null }; 
+			BinaryTreeNode root=CreateBinaryTree(a);
+		}
+		
+		public static BinaryTreeNode CreateBinaryTree(int?[] array)
+		{//use recursive function.
+			if(array[index]!=null)
+			{
+				BinaryTreeNode node=new BinaryTreeNode();
+				node.value=array[index].Value;
+				index++;
+				node.leftChild=CreateBinaryTree(array);
+				index++;
+				node.rightChild=CreateBinaryTree(array);
+				return node;
+			}
+			else {
+				return null;
+			}
+		}
+	}
+	
+	public class BinaryTreeNode
+	{
+		
+		public int value;
+		public BinaryTreeNode leftChild;
+		public BinaryTreeNode	rightChild;
+		
+	}
+}
+
