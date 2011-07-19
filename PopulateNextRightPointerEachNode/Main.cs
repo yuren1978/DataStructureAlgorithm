4907ae8fbd99745a930145a0428e4f8acedbc5f6
Jianwei Sun <jianwei.sun@gmail.com>
2011-07-18 22:59:32 -0400

    populate next right pointer
    populate next right pointer

--- PopulateNextRightPointerEachNode/Main.cs
+++ PopulateNextRightPointerEachNode/Main.cs
@@ -4,24 +4,31 @@ namespace PopulateNextRightPointerEachNode
 {
 	public class Node
 	{
-		int vlaue;
-		Node left;
-		Node right;
-		Node next;
+		public int value;
+		public Node left;
+		public Node right;
+		public Node next;
 	}
 	
 	class MainClass
 	{
 		public static void Main (string[] args)
 		{
-			Node node=null;
-			PopulateNextRightPointerEachNode(node);
+			Node nullNode=null;
+			PopulateNextRightPointerEachNode(nullNode);
+			
+			Node singlNode=new Node(){value=1, left=null, right=null, next=null};
+			PopulateNextRightPointerEachNode(singlNode);
 		}
 		
 		private static void PopulateNextRightPointerEachNode(Node root)
 		{
 			if(root==null)
 				return;
+		
+			if(root.left==null || root.right==null)
+				return;
+			
 		}
 	}
 }
