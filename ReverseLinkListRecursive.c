954d6a6b86ad9540aaf01978697a715f8bb44b2a
Jianwei Sun <jianwei.sun@gmail.com>
2012-03-05 20:27:48 -0500

    riverse link list recrusive

--- ReverseLinkListRecursive.c
+++ ReverseLinkListRecursive.c
@@ -1,5 +1,58 @@
 #include <stdio.h>
 
+typedef struct node{
+	int value;
+	struct node* next;
+} Node;
+
+Node* CreateLinkListFromArray(int* a, int start, int end){
+	Node* head=0;
+	for (int index=start; index<end; index++) {
+		Node* newNode=malloc(sizeof(Node));
+		newNode->value=a[index];
+		if(head==0){
+			newNode->next=0;
+			head=newNode;
+		}
+		else {
+			newNode->next=head;
+			head=newNode;
+		}
+	}
+}
+
+void PrintList(Node* head){
+	Node* travelPointer=head;
+	while(travelPointer!=0){
+		printf("%d\t", travelPointer->value);
+		travelPointer=travelPointer->next;
+	}
+}
+
+Node* ReverseList(Node* head){
+	Node* reveseHead;
+	Node* nextNode;
+	if(head==0)
+		reverseHead=0;		
+	nextNode=head->next;
+	if(nextNode==0){
+		reveseHead=head;	
+	}
+	else {
+		Node* tempNextNode=nextNode;
+		Node* reverseHead=ReverseList(nextNode);
+		tempNextNode->next=head;
+		head->next=0;
+	}	
+	return reverseHead;	
+}
+
+
 int main(int argc, char *argv[]) {
-	
+	int[] a={1,2,3,4,5,6,7,8,9,10};
+	int length=sizeof(a)/sizie(*a);
+	Node* head=CreateLinkListFromArray(a, 0, length-1);
+	PrintList(head);
+	Node* reversedHead= ReverseList(head);
+	PrintList(reverseHead);
 }
