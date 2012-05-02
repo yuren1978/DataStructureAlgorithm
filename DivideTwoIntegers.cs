a314397e15c8c3b3ad31b405935ae9c18e651239
Jianwei Sun <jianwei.sun@gmail.com>
2012-05-01 22:24:39 -0400

    divide two integers

---
+++ DivideTwoIntegers.cs
@@ -0,0 +1,51 @@
+using System;
+
+  
+ public class DivideTwoInteger{
+ 
+ public static int divide(int dividend, int divisor) 
+    {
+        if (divisor == 0)
+            throw new ArithmeticException();
+
+        long a = dividend;
+        long b = divisor;
+        
+        bool neg = false;
+        if (a < 0)
+            neg = !neg;
+        if (b < 0)
+            neg = !neg;
+        a=Math.Abs(a);
+        b=Math.Abs(b);
+        int c = 0;
+        while (b << c <= a){
+               c++;
+               //Console.WriteLine("b :" +b);
+               //Console.WriteLine("c :" +c); 
+        }            
+        int ans = 0;
+        while (c >= 0)
+        {
+            Console.WriteLine("b :"+b+ " c: "+ c + " a: "+a);
+            if (b << c <= a)
+            {
+                a -= b << c;
+                Console.WriteLine("a :"+a);
+                Console.WriteLine("c :"+c);
+
+                ans |= 1 << c;
+                Console.WriteLine("ans :"+ans);
+            }
+            c--;
+        }
+        return neg ? -ans : ans;
+    }
+
+    static void Main(){
+        int answer=divide(-100,3);
+        Console.WriteLine(answer);
+    }
+}
+
+ 
