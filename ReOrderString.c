433e57b152400e682366cd788fb796da65e29e41
Jianwei Sun <jianwei.sun@gmail.com>
2012-05-10 07:50:44 -0400

    add some problems

---
+++ ReOrderString.c
@@ -0,0 +1,4 @@
+//Given a string of lowercase characters, reorder them such that the same characters are at least distance d from each other.
+
+//Input: { a, b, b }, distance = 2
+//Output: { b, a, b }
