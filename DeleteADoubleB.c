9153798d3a52bf62c9a734ae30ae205082ed01d6
Jianwei Sun <jianwei.sun@osumc.edu>
2012-05-21 14:48:48 -0400

    test git

--- DeleteADoubleB.c
+++ DeleteADoubleB.c
@@ -1,5 +1,5 @@
 //处理一个字符串，删除里面所有的A，double所有的B 例子，输入 CAABD, 输出是CBBD
-//要求in space , O (1), no extra memory cost,因为字符串处理变长的空间不算
+//要求in space , O (1), no extra memory cost,因为字符串处理变长的空间不算, facebook
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
