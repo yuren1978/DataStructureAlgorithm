5728f9cf317c1e08d18230f4f2c8620966d02358
Jianwei Sun <jianwei.sun@gmail.com>
2012-05-24 23:04:05 -0400

    delete duplicates from list

--- DeleteDuplicateFromList.c
+++ DeleteDuplicateFromList.c
@@ -1,5 +1,11 @@
+//http://stackoverflow.com/questions/4542600/interview-question-remove-duplicates-from-an-unsorted-linked-list
+//Write code to remove duplicates from an unsorted linked list. FOLLOW UP: How would you solve this problem if a temporary buffer is not allowed?
+
+
+
 int int main(int argc, char const *argv[])
 {
 	/* code */
+	int[] a={1,2,5,4,1,3,2};
 	return 0;
 }
