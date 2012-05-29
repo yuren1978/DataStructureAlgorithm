ff60be2b61a842bd6c7c45663f6c06cde83b935d
Jianwei Sun <jianwei.sun@gmail.com>
2012-05-29 07:14:01 -0400

    add some new problems.

--- DeleteDuplicateFromList.c
+++ DeleteDuplicateFromList.c
@@ -1,11 +1,68 @@
 //http://stackoverflow.com/questions/4542600/interview-question-remove-duplicates-from-an-unsorted-linked-list
 //Write code to remove duplicates from an unsorted linked list. FOLLOW UP: How would you solve this problem if a temporary buffer is not allowed?
 
+#include "stdio.h"
+#include "stdlib.h"
+
+typedef struct node{
+	int value;
+	struct node* next;
+} Node;
 
 
-int int main(int argc, char const *argv[])
+void PrintList(Node* head){
+	Node* temp=head;
+	while(temp!=0){
+		printf("%d\t", temp->value);
+		temp=temp->next;
+	}
+	printf("\n");
+}
+
+void DeleteDuplicateFromList(Node* head){
+	if(head==0)
+		return;	
+	Node* temp=head;
+	while(temp!=0){
+		Node* current=temp;
+		while(current!=0 && current->next!=0){
+			//int nextValue=current->next->value;
+			//printf("next value %d\n", nextValue);
+			if(temp->value==current->next->value){
+				current->next=current->next->next;
+			}
+			else {
+				current=current->next;	
+			}				
+		}
+		temp=temp->next;
+	}			
+}
+
+Node* CreateListFromArray(int* a, int length){
+	Node* head=0;
+	for (int i=0; i<length; i++) {
+		Node* node=malloc(sizeof(Node));
+		node->value=a[i];
+		if(head==0){
+			head=node;
+			head->next=0;
+		}
+		else {
+			node->next=head;
+			head=node;
+		}
+	}
+	return head;	
+}
+
+int main(int argc, char const *argv[])
 {
-	/* code */
-	int[] a={1,2,5,4,1,3,2};
+	int a[]={2,2,2,2,2,2,1,1,1};
+	int length=sizeof(a)/sizeof(*a);
+	Node* head=CreateListFromArray(a, length);
+	PrintList(head);
+	DeleteDuplicateFromList(head);
+	PrintList(head);
 	return 0;
 }
---
+++ mirrorTree.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+typedef struct node{
+	int value;
+	struct node* left;
+	struct node* right;
+} Node;
+
+Node MirrorTree(Node* root){
+	
+}
+
+int main(int argc, char *argv[]) {
+	
+}
