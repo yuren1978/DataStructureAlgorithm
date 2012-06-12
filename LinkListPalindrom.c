8c09d845b275d7a76834ac3f18fb107e266e7c76
Jianwei Sun <jianwei.sun@gmail.com>
2012-06-11 20:38:55 -0400

    some new questions

---
+++ LinkListPalindrom.c
@@ -0,0 +1,61 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//http://www.geeksforgeeks.org/archives/1072
+
+
+typedef enum {false,true} bool;
+
+typedef struct node{
+	int value;
+	struct node* next;
+	
+} Node;
+
+Node* CreateListFromArray(int* a , int length)
+{
+	Node* head=0;
+	for (int index=0; index<length;index++) {
+		Node* newNode=malloc(sizeof(Node));
+		newNode->value=a[index];
+		if(head==0){
+			newNode->next=0;
+			head=newNode;
+		}
+		else {
+			newNode->next=head;
+			head=newNode;
+		}		
+	}
+	return head;
+}
+
+bool isPalindrome(Node** left, Node* right){
+	if(right==NULL)
+		return true;
+		
+	bool isPalin=	isPalindrome(left, right->next);
+	if(isPalin==false)
+		return false;
+		
+	bool isPalin1=(right->value==(*left)->value);
+	
+	*left=(*left)->next;
+	return isPalin1;	
+}
+
+
+int main(int argc, char *argv[]) {
+	int a[]={1,2,3,4,3,2,1};
+	Node* head=CreateListFromArray(a,7);
+	bool isPalin=isPalindrome(&head, head);
+	if(isPalin){
+		printf("\n Palindrome")
+	}else {
+		printf("\n Non-Palindrome")
+	}
+	
+	
+	return 0;	
+}
---
+++ NumberOfElementsInSortedArray.c
@@ -0,0 +1,16 @@
+//http://basicalgos.blogspot.com/2012/02/3.html
+//3. Find the number of an element in a sorted array
+//For instance,  2223333444555,   num_occurrence(4) for this array is  3.
+
+//We use two binary search to find the start and end index of  'num'. The time complexity of this is O(logN). N is the length of the array.
+//To find the boundary, the two binaries search will not stop when an element that equals to 'num' is found. Instead, it continues until the boundary is reached.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int NumberOfOccurance()
+
+int main(int argc, char *argv[]) {
+	
+	return 0;
+}
