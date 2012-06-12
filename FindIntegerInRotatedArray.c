1a3560342cb8bb8f22e9354af096d7b8398c75e3
Jianwei Sun <jianwei.sun@gmail.com>
2012-06-11 23:23:54 -0400

    find an integer in a rotated array

--- FindIntegerInRotatedArray.c
+++ FindIntegerInRotatedArray.c
@@ -2,45 +2,54 @@
 #include "stdlib.h"
 #include "assert.h"
 
-int FindPosInArray(int* a, int length, int value){
-	int posOfIndex=-1;
+
+int FindPosInArray(int* array, int length, int number){
+	int position=-1;
 	int start=0;
 	int end=length-1;
-	while(start<=end){
+	
+	while (start<=end) {
 		int middle=(start+end)/2;
-		if(a[middle]==value){
-			posOfIndex=middle;
+		if(array[middle]==number){
+			position=middle;
 			break;
-		}	
-		else{//not the middle value
-			if(a[middle+1]<a[end]){
-				if(a[middle+1]<=value && a[end]>=value){
+		}
+		else {
+			if(array[middle+1]<array[end]){
+				if(number>=array[middle+1] && number<=array[end]){
 					start=middle+1;
 				}
-				else{
+				else {
 					end=middle-1;
 				}
 			}
-			else{
-				if(a[start]<=value && a[middle-1]>=value){
+			else {
+				if(number<=array[middle-1] && number>=array[0] ){
 					end=middle-1;
 				}
-				else{
+				else {
 					start=middle+1;
 				}
 			}
 		}
 	}
-	return posOfIndex;
+	return position;
 }
+
 
 int main(int argc, char const *argv[])
 {
 
 	int a[]={7,8,9, 1,2,3,4,5,6};
 	int length=sizeof(a)/sizeof(*a);	
-	assert(6==FindPosInArray(a, length,4));
 	assert(0==FindPosInArray(a, length,7));
+	assert(1==FindPosInArray(a, length,8));
+	assert(2==FindPosInArray(a, length,9));
+	assert(3==FindPosInArray(a, length,1));
+	assert(4==FindPosInArray(a, length,2));
+	assert(5==FindPosInArray(a, length,3));
+	assert(6==FindPosInArray(a, length,4));
+	assert(7==FindPosInArray(a, length,5));
 	assert(8==FindPosInArray(a, length,6));
 	return 0;
 }
