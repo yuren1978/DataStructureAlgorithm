75bb72bd57d296e323293f2c28d88d172a833f40
Jianwei Sun <jianwei.sun@gmail.com>
2012-06-24 14:56:47 -0400

    print tree by level

--- PrintTreeByLevel.cs
+++ PrintTreeByLevel.cs
@@ -28,12 +28,84 @@ class TreeByLevel
 		}
 	}
 
+	static void PrintNode(Node root){
+		Console.Write(root.value+" ");
+	}
+
+	static void ZigZapByLevel(Node root){
+		if(root==null)
+				return;
+			bool leftToRight=true;
+			List<Node> list=new List<Node>();
+			Queue<Node> queue=new Queue<Node>();
+			queue.Enqueue(root);
+			int currentLevelNode=1;
+			int nextLevelNode=0;
+			
+			while(queue.Count>0){
+				Node current=queue.Dequeue();
+				currentLevelNode--;	
+					
+				if(current!=null){
+					list.Add(current); 
+					queue.Enqueue(current.left);
+					queue.Enqueue(current.right);
+					nextLevelNode+=2;
+				}
+				
+				if(currentLevelNode==0){
+					if(leftToRight){//left to right
+						leftToRight=false;
+					}
+					else {
+						list.Reverse();
+						leftToRight=true;
+					}
+					list.ForEach(PrintNode);
+					list.Clear();
+					Console.WriteLine(" ");
+					currentLevelNode=nextLevelNode;
+					nextLevelNode=0;
+				}
+			}	
+	}
+
+	static void BFSPrintTreeByLevel(Node root){
+		if(root==null)
+			return;
+			
+		Queue<Node> queue=new Queue<Node>();
+		queue.Enqueue(root);
+		int currentLevelNode=1;
+		int nextLevelNode=0;
+		
+		while(queue.Count>0){
+			Node current=queue.Dequeue();
+			currentLevelNode--;	
+				
+			if(current!=null){
+				Console.Write(current.value+" ");
+				queue.Enqueue(current.left);
+				queue.Enqueue(current.right);
+				nextLevelNode+=2;
+			}
+			
+			if(currentLevelNode==0){
+				Console.WriteLine("");
+				currentLevelNode=nextLevelNode;
+				nextLevelNode=0;
+			}
+		}	
+	}
+
+
 	static void Main()
 	{
 		int[] a={1,2,3,4,5,6,7,8,9};
 		Node root=CreateTree(a,0, a.Length-1);
 		Console.WriteLine("Print Tree By Level");
 		BFSPrintTreeByLevel(root);
+		ZigZapByLevel(root);
 		
 	}
 }
