eafcc7098d15f59ad74d169deb3c6e97e12974ce
Jianwei Sun <jianwei.sun@gmail.com>
2012-07-04 15:36:30 -0400

    binary add numbers

--- 076_BinaryAdd.c
+++ 076_BinaryAdd.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
+#include <assert.h>
+
+int AddNumber(int a, int b){
+	int sum;
+	int carry;
+	do {
+		sum=a^b;
+		carry=(a&b)<<1;
+		
+		a=sum;
+		b=carry;
+	} while (carry>0);
+	return sum;
+}
 
 int main(int argc, char *argv[]) {
-	
+	assert(10==AddNumber(3,7));
+	//assert(4==AddNumber(-3,7));
 }
