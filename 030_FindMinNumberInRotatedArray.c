ba37b873127a9e84b2b00114bd5f8bcf750354c6
Jianwei Sun <jianwei.sun@gmail.com>
2012-07-09 23:35:40 -0400

    find min number in rotated array

--- 030_FindMinNumberInRotatedArray.c
+++ 030_FindMinNumberInRotatedArray.c
@@ -2,6 +2,37 @@
 #include <stdlib.h>
 #include <assert.h>
 
+
+int FindMinNumber(int* array, int length){
+	int start=0, end=length-1;
+	while (start<end) {
+		int middle=(start+end)/2;
+		if(array[start]<array[middle]){
+			if(array[start]<array[end] && array[start]<array[middle+1]){
+				end=middle;
+			}
+			else {
+				start=middle+1;
+			}	
+		}
+		else {
+			if(array[middle+1]<array[start] && array[middle+1]<array[middle]){
+				start=middle+1;	
+			}
+			else {
+				end=middle;
+			}
+		}
+	}
+	return array[start];	
+}
+
 int main(int argc, char *argv[]) {
 	int a[]={4,5,6,7,8,9,10, 1,2,3};
+	int length=sizeof(a)/sizeof(*a);
+	assert(1==FindMinNumber(a, length));
+	int b[]={1,2,3,4,5,6,7,8,9,10};
+	length=sizeof(b)/sizeof(*b);
+	assert(1==FindMinNumber(b, length));
+	
 }
