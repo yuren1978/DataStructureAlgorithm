341a81780f984b436d3b4d11bb3d52a0892fbc18
Jianwei Sun <jianwei.sun@gmail.com>
2012-07-12 23:00:51 -0400

    Determine if string is a valid number

---
+++ 001_IsStringValidNumber.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+//http://www.mitbbs.com/article_t/JobHunting/32161845.html
+
+typedef enum {false, true} bool;
+
+bool DetermineIfStringValidNumber(const char* string){
+	return false;
+}
+
+int main(int argc, char *argv[]) {
+	
+}
