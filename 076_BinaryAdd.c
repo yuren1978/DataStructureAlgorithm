4f04a919fa2436a24af78bc9e46ab488b26df7a3
Jianwei Sun <jianwei.sun@gmail.com>
2012-08-01 21:46:38 -0400

    update some questions

--- 076_BinaryAdd.c
+++ 076_BinaryAdd.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <assert.h>
 
-unsigned int AddNumber(unsigned int a, unsigned int b){
+int AddNumber(int a, int b){
 	int sum;
 	int carry;
 	do {
@@ -14,7 +14,27 @@ unsigned int AddNumber(unsigned int a, unsigned int b){
 	return sum;
 }
 
+int AddWithoutArithmetic(int num1, int num2)
+{
+        if(num2 == 0)
+                return num1;
+ 
+        int sum = num1 ^ num2;
+        int carry = (num1 & num2) << 1;
+ 
+        return AddWithoutArithmetic(sum, carry);
+}
+
+int add(int a,int b){
+    char * c;
+    c = (char *) a;
+    return &c[b];
+}
+
+
 int main(int argc, char *argv[]) {
 	assert(10==AddNumber(3,7));
-	//assert(4==AddNumber(-3,7));
+	assert(10==AddWithoutArithmetic(3,7));
+	assert(4==AddWithoutArithmetic(-3,7));
+	assert(4==add(-3,7));
 }
