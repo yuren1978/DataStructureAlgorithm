d08f1886b5ee4eaaa669b51d0c89d2b15c100b69
Jianwei Sun <jianwei.sun@gmail.com>
2012-08-04 16:11:00 -0400

    update question

--- 090_FindMinWindowContailAllElements.c
+++ 090_FindMinWindowContailAllElements.c
@@ -1,4 +1,12 @@
 //http://www.leetcode.com/2010/11/finding-minimum-window-in-s-which.html
+//Given a set T of characters and a string S, find the minimum window in S which will contain all the characters in T in complexity O(n).
+//eg,
+//S = “ADOBECODEBANC”
+//T = “ABC”
+
+//Minimum window is “BANC”.
+
+
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
