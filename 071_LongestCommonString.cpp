110cc1ec40de359f8d6ed4a4c9184119ff2c94f6
Jianwei Sun <jianwei.sun@gmail.com>
2012-08-11 13:53:03 -0400

    add new problems

---
+++ 071_LongestCommonString.cpp
@@ -0,0 +1,12 @@
+//http://zhedahht.blog.163.com/blog/static/254111742007376431815/
+//：如果字符串一的所有字符按其在字符串中的顺序出现在另外一个字符串二中，则字符串一称之为字符串二的子串。注意，并不要求子串（字符串一）的字符必须连续出现在字符串二中。请编写一个函数，输入两个字符串，求它们的最长公共子串，并打印出最长公共子串。
+//例如：输入两个字符串BDCABA和ABCBDAB，字符串BCBA和BDAB都是是它们的最长公共子串，则输出它们的长度4，并打印任意一个子串。
+
+//分析：求最长公共子串（Longest Common Subsequence, LCS）是一道非常经典的动态规划题，因此一些重视算法的公司像MicroStrategy都把它当作面试题。
+
+#include <iostream>
+
+using namespace std;
+int main(int argc, char *argv[]) {
+	
+}
