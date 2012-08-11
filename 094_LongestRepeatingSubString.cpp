2d1288757f3367ca8bc6cd0891bb8a0f106b4814
Jianwei Sun <jianwei.sun@gmail.com>
2012-08-11 16:12:29 -0400

    update some questions

---
+++ 094_LongestRepeatingSubString.cpp
@@ -0,0 +1,7 @@
+//http://www.mitbbs.com/article_t1/JobHunting/32189139_0_1.html
+#include <iostream>
+
+using namespace std;
+int main(int argc, char *argv[]) {
+	
+}
---
+++ 101_LongestValidParenthesis.cpp
@@ -0,0 +1,13 @@
+//http://www.mitbbs.com/article_t/JobHunting/32185871.html
+//Given a string containing just the characters '(' and ')', find the length of the longest valid (well-formed) parentheses substring.
+//For "(()", the longest valid parentheses substring is "()", which has length = 2.
+//Another example is ")()())", where the longest valid parentheses substring is "()()", which has length = 4.
+
+
+
+#include <iostream>
+
+using namespace std;
+int main(int argc, char *argv[]) {
+	
+}
