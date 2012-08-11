662cce6ec9f06ec1b9cc88227133d8d2e29050f8
Jianwei Sun <jianwei.sun@gmail.com>
2012-08-11 14:03:54 -0400

    update questions

--- 049_ReOrderString.c
+++ 049_ReOrderString.c
@@ -1,3 +1,4 @@
+//http://www.leetcode.com/2010/05/here-is-another-google-phone-interview.html?utm_source=feedburner&utm_medium=feed&utm_campaign=Feed%3A+ihas1337code+%28LeetCode%29
 //Given a string of lowercase characters, reorder them such that the same characters are at least distance d from each other.
 
 //Input: { a, b, b }, distance = 2
---
+++ 084_coinsInline.cpp
@@ -0,0 +1,12 @@
+//http://www.leetcode.com/2011/02/coins-in-line.html?utm_source=feedburner&utm_medium=feed&utm_campaign=Feed%3A+ihas1337code+%28LeetCode%29
+
+//There are n coins in a line. (Assume n is even). Two players take turns to take a coin from one of the ends of the line until there are no more coins left. The player with the larger amount of money wins.
+//Would you rather go first or second? Does it matter?
+//Assume that you go first, describe an algorithm to compute the maximum amount of money you can win.
+
+#include <iostream>
+
+using namespace std;
+int main(int argc, char *argv[]) {
+	
+}
