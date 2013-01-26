52a2e006e9f78f473a0156c731e3aa463e7812fe
Jianwei Sun <jianwei.sun@gmail.com>
2013-01-26 17:11:11 -0500

    Find median of two sorted array

---
+++ L002-FindMedianTwoSortedArray.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+//http://www.mitbbs.com/article_t1/JobHunting/32275979_0_1.html
+using namespace std;
+
+double findKth(int a[], int m, int b[], int n, int k)
+{
+	if(m>n)
+		findKth(b,n, a,m , k);
+		
+	if(m==0)
+		return b[k-1];
+		
+	if(k==1)
+		return min(a[0], b[0]);
+	
+	int pa = min(k/2, m), pb = k - pa;
+	
+	if (a[pa-1] < b[pb-1]) {
+		return findKth(a+pa, m-pa, b, n, k - pa);
+	}
+	else {
+		return findKth(a, m, b+pb, n-pb, k-pb);
+	}				
+}
+
+double findMedianSortedArrays(int A[], int m, int B[], int n){
+	int total=m+n;
+	if(total & 0x1){//odd
+		return findKth(A, m, B, n, total/2+1);
+	}
+	else {//evens
+		return ( findKth(A, m, B, n, total/2) + findKth(A, m, B, n, total/2+1) )/2;
+	}
+}
+
+int main(int argc, char *argv[]) {
+	
+}
