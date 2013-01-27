2fc00cbb0b94394260bf2981e747cb4ad7879036
Jianwei Sun <jianwei.sun@gmail.com>
2013-01-27 12:50:48 -0500

    leetcode problem

--- L002-FindMedianTwoSortedArray.cpp
+++ L002-FindMedianTwoSortedArray.cpp
@@ -2,26 +2,18 @@
 //http://www.mitbbs.com/article_t1/JobHunting/32275979_0_1.html
 using namespace std;
 
+
 double findKth(int a[], int m, int b[], int n, int k)
 {
-	if(m>n)
-		findKth(b,n, a,m , k);
-		
-	if(m==0)
-		return b[k-1];
-		
-	if(k==1)
-		return min(a[0], b[0]);
-	
-	int pa = min(k/2, m), pb = k - pa;
-	
-	if (a[pa-1] < b[pb-1]) {
-		return findKth(a+pa, m-pa, b, n, k - pa);
-	}
-	else {
-		return findKth(a, m, b+pb, n-pb, k-pb);
-	}				
+    if (m > n) return findKth(b, n, a, m, k);
+
+    if (m == 0) return b[k-1];
+    if (k == 1) return min(a[0], b[0]);
+    int pa = min(k/2, m), pb = k - pa;
+    if (a[pa-1] < b[pb-1]) return findKth(a+pa, m-pa, b, n, k - pa);
+    return findKth(a, m, b+pb, n-pb, k-pb);
 }
+
 
 double findMedianSortedArrays(int A[], int m, int B[], int n){
 	int total=m+n;
