0dc585ce6cb8d231b6d17e9b2cf50265f6e24109
Jianwei Sun <jianwei.sun@gmail.com>
2013-02-16 11:56:27 -0500

    inverse number coun t

--- Standford_P1_InverseNumber.cpp
+++ Standford_P1_InverseNumber.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <fstream>
 #include <cassert>
+#include <vector>
 
 using namespace std;
 
@@ -9,7 +11,7 @@ void Swap(int* numbers, int start, int end){
 	numbers[end]=temp;
 }
 
-int CountInverse(int* numbers, int startIndex, int endIndex, int number){
+long long CountInverse(int* numbers, int startIndex, int endIndex, int number){
 	for (int i=startIndex; i<=endIndex; ++i) {
 		int firstNumber=numbers[i];
 		if(firstNumber>number){
@@ -19,8 +21,8 @@ int CountInverse(int* numbers, int startIndex, int endIndex, int number){
 	return 0;
 }
 
-int CrossInvesePairs(int* numbers, int firstStartIndex, int firstEndIndex, int secondStartIndex, int secondEndIndex){
-	int inverseCount=0;
+long long CrossInvesePairs(int* numbers, int firstStartIndex, int firstEndIndex, int secondStartIndex, int secondEndIndex){
+	long long inverseCount=0;
 	for (int i=secondStartIndex; i<=secondEndIndex; ++i) {
 		int secondNumber=numbers[i];
 		inverseCount+=CountInverse(numbers,firstStartIndex,firstEndIndex, secondNumber);
@@ -28,7 +30,43 @@ int CrossInvesePairs(int* numbers, int firstStartIndex, int firstEndIndex, int s
 	return inverseCount;
 }
 
-int SortCountInvesePairs(int* numbers, int start, int end){
+void Merge(int* numbers,int firstStartIndex, int firstEndIndex, int secondStartIndex, int secondEndIndex ){
+	//cout<<firstStartIndex<<endl;
+	//cout<<secondEndIndex<<endl;
+	int* temp=new int[secondEndIndex-firstStartIndex+1];
+	int tempIndex=0;
+	int* p1=numbers+firstStartIndex; int* p1End=numbers+firstEndIndex+1;
+	int* p2=numbers+secondStartIndex; int* p2End=numbers+secondEndIndex+1;
+	while (p1!=p1End && p2!=p2End) {
+		if(*p1<*p2){
+			temp[tempIndex++]=*p1;
+			++p1;
+		}
+		else {
+			temp[tempIndex++]=*p2;
+			++p2;			
+		}
+	}
+	if(p1==p1End){
+		while (p2!=p2End) {
+			temp[tempIndex++]=*p2;
+			++p2;			
+		}
+	}
+	else {
+		while (p1!=p1End) {
+			temp[tempIndex++]=*p1;
+			++p1;
+		}
+	}
+	assert(tempIndex==(secondEndIndex-firstStartIndex+1));
+	for (int i=firstStartIndex; i<=secondEndIndex;++i) {
+		numbers[i]=temp[i-firstStartIndex];
+	}	
+	delete[] temp;
+}
+
+long long SortCountInvesePairs(int* numbers, int start, int end){
 	if(start==end){
 		return 0;
 	}
@@ -40,27 +78,28 @@ int SortCountInvesePairs(int* numbers, int start, int end){
 		return inverseCount;
 	}
 	int middle=(start+end)/2;
-	int firstHalf=SortCountInvesePairs(numbers, start, middle);
-	int secondHalf=SortCountInvesePairs(numbers, middle+1, end);
-	int cross=CrossInvesePairs(numbers, start, middle, middle+1, end);
+	long long firstHalf=SortCountInvesePairs(numbers, start, middle);
+	long long secondHalf=SortCountInvesePairs(numbers, middle+1, end);
+	long long cross=CrossInvesePairs(numbers, start, middle, middle+1, end);
+	Merge(numbers, start, middle, middle+1, end);
 	return firstHalf+secondHalf+cross;
 }
 
 
-int CountInvesePairs(int* numbers, int n){
+long long CountInvesePairs(int* numbers, int n){
 	return SortCountInvesePairs(numbers, 0, n-1);
 }
 
 
 int main(int argc, char *argv[]) {
-	//int a[]={1};
-	//assert(0==CountInvesePairs(a,1)); 
-	//int b[]={1,2};
-	//assert(0==CountInvesePairs(b,2));
-	//int c[]={2,1};
-	//assert(1==CountInvesePairs(c,2));
-	//int d[]={1,1};
-	//assert(0==CountInvesePairs(d,2));
-	int e[]={4,3,1,2};
-	assert(5==CountInvesePairs(e,4));
+	ifstream fin("/Users/jianweisun/Dropbox/IntegerArray.txt");
+	string line;
+	vector<int> v;
+	while (getline(fin, line)) {
+		v.push_back(atoi(line.c_str()));
+	}
+	cout<<v.size()<<endl;
+	cout<<CountInvesePairs(&v[0], v.size());
+	//int e[]={3, 7, 10, 14, 18,19, 2,11,16, 17, 23, 25 };
+	//assert(13==CountInvesePairs(e,12));
 }
