10bf1cfe7953632530069a521944d5f4cf866b69
Jianwei Sun <jianwei.sun@osumc.edu>
2014-02-20 17:28:45 -0500

    organize problems into different folders
    organize problems into different folders

--- 067_UniqueCharacterInTwoString.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-//两个字符串，求出unique characters，即只出现在一个string中的cha（array[26]，用0-3标记）
-int main(int argc, char *argv[]) {
-	const char* firstString="abcdefhjkm";
-	const char* secondString="defghkft";
-	return 0;
-}
