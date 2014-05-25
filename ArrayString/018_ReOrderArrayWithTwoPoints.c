eea0a0129a0bc50f5a2b304dbafe919062641579
Jianwei Sun <jianwei.sun@osumc.edu>
2014-05-25 13:12:54 -0400

    delete known problems

--- ArrayString/018_ReOrderArrayWithTwoPoints.c
+++
@@ -1,6 +0,0 @@
-//from he hao tao book
-#include <stdio.h>
-
-int main(int argc, char *argv[]) {
-	
-}
--- Number/017_FindIntegerInRotatedArray.c
+++ Number/017_FindIntegerInRotatedArray.c
@@ -3,24 +3,39 @@
 #include "assert.h"
 
 
-int FindPosInArray(int* array, int length, int number){
+ bool between(int A[], int l, int r, int target){
+        return target>=A[l] && target<=A[r];
+    }
 
+int FindPosInArray(int A[], int n, int target){
+ 		int l=0, r=n-1;
+        while(l<r){
+            int m=l+(r-l)/2;
+            
+            //printf("middle value %d\n", A[m] );
+            if(target==A[m]) return m;
+            
+            if(A[l]<=A[m] && between(A, l, m, target)){
+            	printf("go left\n");
+                r=m-1;
+            }
+            else{
+            	printf("go right\n");
+                l=m+1;
+            }
+
+            printf("%d - %d\n", l, r );
+        }
+        
+        return target==A[l]?l:-1;
 }
 
 
 int main(int argc, char const *argv[])
 {
 
-	int a[]={7,8,9, 1,2,3,4,5,6};
+	int a[]={5,1,3};
 	int length=sizeof(a)/sizeof(*a);	
-	assert(0==FindPosInArray(a, length,7));
-	assert(1==FindPosInArray(a, length,8));
-	assert(2==FindPosInArray(a, length,9));
-	assert(3==FindPosInArray(a, length,1));
-	assert(4==FindPosInArray(a, length,2));
-	assert(5==FindPosInArray(a, length,3));
-	assert(6==FindPosInArray(a, length,4));
-	assert(7==FindPosInArray(a, length,5));
-	assert(8==FindPosInArray(a, length,6));
+	assert(0==FindPosInArray(a, length, 5));
 	return 0;
 }
