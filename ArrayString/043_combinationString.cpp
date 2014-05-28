97d3b7b7cf62a7da052ae4145db0743cbd2100a0
Jianwei Sun <jianwei.sun@osumc.edu>
2014-05-28 18:48:05 -0400

    delete some problems

--- ArrayString/043_combinationString.cpp
+++ ArrayString/043_combinationString.cpp
@@ -1,5 +1,6 @@
 //http://zhedahht.blog.163.com/blog/static/2541117420114172812217/
 #include <cstdlib>
+#include <cstdio>
 #include <iostream>
 #include <cmath>
 #include <vector>
@@ -7,24 +8,19 @@
 using namespace std;
 
 //用一个数组，模拟2进制加法器，某一个为1，则取对应的字符，若为0则不取，就能够实现字符组合。
+//a,b,c, ab, ac, bc, abc
 
-void CombinationNumber(const int* cur, int length)
+void CombinationNumber(const char* cur, int length)
 {
 	int number=std::pow(2.0,length);
 		for (int i=1; i<number; i++) {
 			for (int j=0; j<length; j++) {
-				if((i>>j)&1){
-					printf("%d ", *(cur+j));
+				if((i>>j)&1){ //understand this condition
+					printf("%c ", *(cur+j));
 				}
 			}
 			printf("\n");
 		}
-
-}
-
-void Combination2(const char* cur, int length)
-{
-
 }
 
 void Combination(const char* cur, vector<char>& result)
@@ -45,12 +41,12 @@ void Combination(const char* cur, vector<char>& result)
 
 
 int main(int argc, char *argv[]) {
-	const char* test="abc";
+	const char* a="abc";
 	//Combination2(test,strlen(test));
-	vector<char> vec;
-	Combination(test, vec);
+	//vector<char> vec;
+	//Combination(test, vec);
 	//int a[]={1,2,3,4};
-	//CombinationNumber(a,4);
+	CombinationNumber(a,3);
 	return 0;
 }
 
--- ArrayString/087_stringReplacement.c
+++
@@ -1,9 +0,0 @@
-//http://www.leetcode.com/2010/11/microsoft-string-replacement-problem.html
-//Replace all occurrence of the given pattern to ‘X’.
-//For example, given that the pattern=”abc”, replace “abcdeffdfegabcabc” with “XdeffdfegX”. Note that multiple occurrences of abc’s that are contiguous will be replaced with only one ‘X’.
-
-#include <stdio.h>
-
-int main(int argc, char *argv[]) {
-	
-}
