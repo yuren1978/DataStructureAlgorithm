c0b713c1950d79960b1370b58ea254371fcc790d
Jianwei Sun <jianwei.sun@osumc.edu>
2014-06-02 21:33:38 -0400

    delete nodes

---
+++ ArrayString/textJustify.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+vector<string> fullJustify(vector<string> &words, int L) {
+	int wordCount=words.size();
+	int lineStart=0;
+	int lineEnd=0;
+	while(getLine(lineStart, lineEnd)>=L && ){
+
+		lineStart=lineEnd;
+	}
+}
