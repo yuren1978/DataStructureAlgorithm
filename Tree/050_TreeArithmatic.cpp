e40e0262c60fc508d6d6faff1659229babfad857
Jianwei Sun <jianwei.sun@osumc.edu>
2014-06-12 16:00:14 -0400

    delete some problems

--- Tree/050_TreeArithmatic.cpp
+++
@@ -1,7 +0,0 @@
-//https://docs.google.com/spreadsheet/pub?key=0AqHq2JLBVNBwdHJkVEFuRWExT2owWGNKOXM4WnpQZEE&single=true&gid=0&output=html
-#include <iostream>
-
-using namespace std;
-int main(int argc, char *argv[]) {
-	
-}
