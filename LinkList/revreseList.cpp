fc9905cbd5f324a91751d78912ef28b4dadab4bd
Jianwei Sun <jianwei.sun@gmail.com>
2014-09-14 10:46:57 -0400

    update problems

--- LinkList/revreseList.cpp
+++ LinkList/revreseList.cpp
@@ -46,6 +46,34 @@ Node* reverse(Node* head){
 	return prev;
 }
 
+Node * mergeTwoLists(Node *l1, Node *l2) {
+        if(NULL==l1) return l2;
+        if(NULL==l2) return l1;
+        
+        Node* dummy=new Node(0);
+        Node* tail=dummy;
+        while(l1!=NULL && l2!=NULL){
+            if(l1->value<=l2->value){
+                tail->next=l1;
+                tail=l1;
+                l1++;
+            }
+            else{
+            	cout<<"come here"<<endl;
+                tail->next=l2;
+                tail=l2;
+                l2++;
+            }
+        }
+        cout<<"come here"<<endl;
+        if(l1)
+            tail->next=l1;
+        else
+            tail->next=l2;
+            
+        return dummy->next;        
+    }
+
 void printList(Node* head){
 	Node* current=head;
 	while(current){
@@ -55,12 +83,18 @@ void printList(Node* head){
 	cout<<endl;
 }
 
+
+
 int main(int argc, char const *argv[])
 {
-	Node* head=createList();
-	printList(head);
-	Node* reverseHead=reverseRcursively(head);
-	printList(reverseHead);	
+	//Node* head=createList();
+	//printList(head);
+	//Node* reverseHead=reverseRcursively(head);
+	//printList(reverseHead);	
+
+	Node* l1=new Node(1);
+	Node* l2=new Node(2);
+	Node* m=mergeTwoLists(l1,l2);
 	return 0;
 }
 
--- Matrix/getConnectedCount.cpp
+++ Matrix/getConnectedCount.cpp
@@ -8,28 +8,31 @@ bool inRange(int i, int j, int m, int n){
 	return i>=0 && i<m && j>=0 && j<n ;
 }
 
-void dfs(const vector< vector<int> >& v , vector< vector<bool> >& mark,  int m, int n, int i, int j){
+void dfs(vector< vector<int> >& v , vector< vector<bool> >& mark,  int m, int n, int i, int j, const int& count){
 	if(!inRange(i,j,m,n) || mark[i][j])
 		return;
 
 	if( v[i][j]==1){
 		mark[i][j]=true;
-		dfs(v, mark, m, n, i-1, j);
-		dfs(v, mark, m, n, i, j-1);
-		dfs(v, mark, m, n, i+1, j);
-		dfs(v, mark, m, n, i, j+1);
+		v[i][j]=count;
+		dfs(v, mark, m, n, i-1, j, count);
+		dfs(v, mark, m, n, i, j-1, count);
+		dfs(v, mark, m, n, i+1, j, count);
+		dfs(v, mark, m, n, i, j+1, count);
 	}	
 }
 
 
-int getConnectedCount(const vector< vector<int> >& v , int m, int n){
+int getConnectedCount(vector< vector<int> >& v ){
 	
-	int count=0;
+	int m=v.size();
+	int n=v[0].size();
+	int count=1;
 	vector< vector<bool> > mark(m, vector<bool>(n, false));
 	for (int i = 0; i < m; ++i){
 		for(int j=0; j<n; ++j){
 			if(v[i][j]==1 && !mark[i][j]){
-				dfs(v, mark,m, n, i, j);
+				dfs(v, mark,m, n, i, j, count);
 				count++;
 			}	
 		}
@@ -37,39 +40,31 @@ int getConnectedCount(const vector< vector<int> >& v , int m, int n){
 	return count;
 }
 
+void printmaxtirx(const vector< vector<int> >& v ){
+	int m=v.size();
+	int n=v[0].size();
+	for(int i=0; i<m; i++){
+		cout<<"{";
+		for(int j=0; j<n;j++){
+			cout<<v[i][j];
+			if(j!=n-1)
+				cout<<",";
+		}
+		cout<<"}"<<endl;
+	}
+}
 
 int main(int argc, char const *argv[])
 {
-	vector< vector<int> > a =
-	{
-	    {1,0,1},
-	    {0,1,0},
-	    {1,1,1}
-	};
-
-	vector< vector<int> > b =
-	{
-	    {1,1,1},
-	    {0,0,1},
-	    {1,1,1}
-	};
-
 
 	vector< vector<int> > c =
 	{
-	    {1,1,1,1,1,1,1,1,1,1,1,1},
-	    {0,0,0,0,0,0,0,0,0,0,0,1},
-	    {1,1,1,0,0,0,0,0,0,0,0,1},
-	    {1,0,0,0,0,0,1,0,0,0,0,1},
-	    {1,0,0,0,0,0,0,0,0,0,0,1},
-	    {1,0,0,0,0,0,0,0,0,0,0,1},
-		{1,1,1,1,1,1,1,1,1,1,1,1}
+	    {0, 0, 0, 0, 0, 1, 1, 0, 0, 0},
+	    {1, 1, 0, 0, 1, 1, 0, 0, 0, 0},
+	    {1, 1, 1, 0, 0, 1, 1, 0, 0, 0},
+	    {0, 0, 0, 1, 1, 0, 0, 0, 0, 0 }
 	};
-
-
-
-	cout<<getConnectedCount(a,3,3)<<endl;
-	cout<<getConnectedCount(b,3,3)<<endl;
-	cout<<getConnectedCount(c,7,12)<<endl;
+	getConnectedCount(c);
+	printmaxtirx(c);
 	return 0;
 }
